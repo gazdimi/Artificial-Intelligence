2a838d9b7d248aadb325c4f4047429207d809b33
dimitra <p16178@students.cs.unipi.gr>
2020-10-10 17:21:17 +0300

    Initial state done with data and positions

--- Blocks_world_breadth_first_search.py
+++ Blocks_world_breadth_first_search.py
@@ -12,11 +12,11 @@ def print_state(list):
         else:
             print(spaces,"| ", i,"|")
 
-def reconstruct_initial(n):
+def reconstruct_initial(n):                                                         #get random block positions for blocks to be placed
     first, middle, last = [0], list(range(1, n)), [n]
     all_combinations = (comb for i in range(n) for comb in combinations(middle, i))
     list_combinations =  list(list(map(sub, chain(a, last), chain(first, a))) for a in all_combinations)
-    return (list_combinations[randint(0,len(list_combinations))])
+    return (list_combinations[randint(0,len(list_combinations)-1)])
 
 #-------------------------------Main program-----------------------------------
 print("\nSolution for Blocks World problem using breadth first search algorithm")
@@ -31,13 +31,25 @@ while True:
         print("Error type of input, only integers are allowed. Please try again...")
 
 goal_state = [i for i in range(1,blocks+1)]
-initial= sample(goal_state,blocks)
+initial= sample(goal_state,blocks)                                                  #initial block numbers
+initial_positions = reconstruct_initial(len(initial))                               #random position for initial blocks to be placed on the ground
 
-print("initial state: ", initial)
-print("goal state: ", goal_state)
+initial_state = []                                                                  #will store initial_state blocks (in random positions)
+previous_temp = []
+for i in initial_positions:
+    temp = []
+    counter = i
+    for j in initial:
+        flat_list = [item for sublist in previous_temp for item in sublist]
+        if (j in flat_list):
+            continue
+        if(counter!=0):
+            temp.append(j)
+            counter-=1
+        else:
+            break
+    initial_state.append(temp)
+    previous_temp.append(temp)
 
-'''print_state(initial_state)
-print("GROUND ----------------------------------------------------")'''
-
-initial_state = reconstruct_initial(len(initial))
-print(initial_state)
+#print_state(initial_state)
+#print("GROUND ----------------------------------------------------")
